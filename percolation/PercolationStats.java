f44cb459809d8434dcecf431c7fca6cb4b701941
stefano nepa <stefano nepa>
2016-12-29 18:53:23 +0100

    add gitignore, add Class Stats, update README

---
+++ percolation/PercolationStats.java
@@ -0,0 +1,25 @@
+public class PercolationStats {
+	   public PercolationStats(int n, int trials) {
+		   // perform trials independent experiments on an n-by-n grid
+	   }
+	   public double mean(){
+		   // sample mean of percolation threshold
+		   return 0.0;
+	   }
+	   public double stddev(){
+		   // sample standard deviation of percolation threshold
+		   return 0.0;
+	   }
+	   public double confidenceLo(){
+		   // low  endpoint of 95% confidence interval
+		   return 0.0;
+	   }
+	   public double confidenceHi(){
+		   // high endpoint of 95% confidence interval
+		   return 0.0;
+	   }
+
+	   public static void main(String[] args){
+		   // test client (described below)
+	   }
+	}
--- percolation/README.md
+++ percolation/README.md
@@ -1,3 +1,4 @@
 # Percolation
 
 - First excercise: http://coursera.cs.princeton.edu/algs4/assignments/percolation.html
+- Download algs4.jar in the root directory (percolation)
